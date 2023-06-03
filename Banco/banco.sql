4bd08116e60c7ea377790e886a01d43cbd0e9904
Higor <higor.a@aluno.ifsp.edu.br>
2023-06-03 15:18:29 -0300

    gjghj

---
+++ Banco/banco.sql
@@ -0,0 +1,35 @@
+-- arquivo de criação de banco de dados
+
+DROP  DATABASE IF EXISTS projeto1;
+CREATE DATABASE projeto1;
+
+USE projeto1;
+
+CREATE TABLE agenda 
+(
+	id_agenda int not null auto_increment,
+    
+);
+
+-- INSERIR DADOS
+
+/*
+
+USE projeto1;
+ 
+-- ISNERIR DADOS
+
+INSERT INTO usuario (nome_usuario, fone_usuario, email_usuario, senha_usuario) values ('CASSIO', '(18)3622-9046','cassio@mail.com','1234');
+
+-- ALTERAR DADOS
+SELECT * FROM usuario;
+
+*/
+
+
+-- ALTERAR DADOS
+
+/*
+UPDATE usuario SET nome_usuario="Zezinho", email_usuario = "ze.gmail.com"
+WHERE id_usuario=2;
+*/
